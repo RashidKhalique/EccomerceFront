7d036fc0d1125af8e208780de4013df61eec730c
RashidKhalique <rashidkhanjamali26@gmail.com>
2024-11-18 13:14:24 +0500

    Merge

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // If the module is in node_modules, we can split it into a vendor chunk
+          if (id.includes('node_modules')) {
+            return 'vendor' // all dependencies go into the vendor chunk
+          }
+        }
+      }
+    }
+  }
 })
