008a904c7dc3bb8feb06d97a6a010d56db529388
Rashid Khalique <91251671+RashidKhalique@users.noreply.github.com>
2025-04-09 21:32:27 +0500

    Update UserHome.jsx1

--- src/Pages/userPages/UserHome.jsx
+++ src/Pages/userPages/UserHome.jsx
@@ -131,10 +131,10 @@ function UserHome() {
                     <div className="flex items-center space-x-6 lg:space-x-8">
                       <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
                         <div className="h-64 w-44 overflow-hidden rounded-lg sm:opacity-0 lg:opacity-100">
-                          <img src="https://tailwindui.com/plus/img/ecommerce-images/home-page-03-hero-image-tile-01.jpg" alt="" className="h-full w-full object-cover object-center" />
+                          <img src="https://carmellamarketing.com/wp-content/uploads/2023/05/Screen-Shot-2023-05-09-at-9.50.02-AM-1024x650.png" alt="" className="h-full w-full object-cover object-center" />
                         </div>
                         <div className="h-64 w-44 overflow-hidden rounded-lg">
-                          <img src="https://tailwindui.com/plus/img/ecommerce-images/home-page-03-hero-image-tile-02.jpg" alt="" className="h-full w-full object-cover object-center" />
+                          <img src="https://voyado.com/wp-content/uploads/2022/08/beauty-ecommerce-products.jpg" alt="" className="h-full w-full object-cover object-center" />
                         </div>
                       </div>
                       <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
