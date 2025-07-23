184aa135dfcd05ea91954e5846a74db91775465c
RashidKhalique <rashidkhanjamali26@gmail.com>
2025-07-23 16:58:29 +0500

    All4

--- src/Pages/userPages/UserHome.jsx
+++ src/Pages/userPages/UserHome.jsx
@@ -48,11 +48,11 @@ function UserHome() {
           </div>
           </section>
 
+       <PromotionalSection/>
 
       {/* Categories Section */}
      <Category/>
 
-       <PromotionalSection/>
 
       {/* Testimonials Section */}
       <section className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:px-8">
